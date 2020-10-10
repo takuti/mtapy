06c62eb0fc9bd10a1fb3ce431fdee2d8373cefac
Takuya Kitazawa <k.takuti@gmail.com>
2020-10-10 09:09:55 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mtapy',
+    version='0.0.1',
+    description='A library for multi-touch attribution',
+    author='Takuya Kitazawa',
+    author_email='k.takuti@gmail.com',
+    license='MIT',
+    url='https://github.com/takuti/mtapy',
+    packages=find_packages(exclude=['*tests*']),
+)
